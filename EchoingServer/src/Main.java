c05d8b3f88d30e3ba8e32fd75f04536fa9838e50
farhadahmed <amillermarsupial@gmail.com>
2021-02-04 02:15:55 -0800

    create server that listens on port 5000 and responds to messages sent by a client

---
+++ EchoingServer/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Create server socket that listens on port 5000.
+        try(ServerSocket serverSocket = new ServerSocket(5000)) {
+
+            // Because this is an infinite loop, the server will only terminate when manually terminated.
+            while (true) {
+                // Listens for and accepts connections to the socket at the specified port.
+                new Echo(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR server IOException: " + e.getMessage());
+        }
+    }
+}
