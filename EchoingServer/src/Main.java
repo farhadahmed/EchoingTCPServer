2a1d45842b28e3d20e5c4b75c8121e2bb3d8fa17
farhadahmed <farhadahmed2007@gmail.com>
2021-02-04 10:51:28 -0800

    use variable to set server port

--- EchoingServer/src/Main.java
+++ EchoingServer/src/Main.java
@@ -3,10 +3,11 @@ import java.net.ServerSocket;
 
 public class Main {
     public static void main(String[] args) {
+        int port = 5000;
 
         // Create server socket that listens on port 5000.
-        try(ServerSocket serverSocket = new ServerSocket(5000)) {
-
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server started at port " + port);
             // Because this is an infinite loop, the server will only terminate when manually terminated.
             while (true) {
                 // Listens for and accepts connections to the socket at the specified port.
